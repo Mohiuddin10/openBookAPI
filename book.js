a9173b83760342e87661fef9e5936bdc6e32943f
Mohiuddin10 <sj.ctg6737@gmail.com>
2023-04-03 00:13:15 +0600

    old data clear

--- book.js
+++ book.js
@@ -1,6 +1,8 @@
 const loadBook = () => {
     const searchName = document.getElementById('bookName');
     const searchValue = searchName.value;
+    const searchResult = document.getElementById('bookShow');
+    searchResult.innerHTML = '';
 
     fetch(`https://openlibrary.org/search.json?q=${searchValue}`)
     .then((response) => response.json())
@@ -10,26 +12,48 @@ const loadBook = () => {
 
 const showBook =(data) => {
     const {numFound, docs} = data;
+    // show number of book found 
+    const countBook = document.getElementById('bookNum');
+    countBook.innerHTML = `Total book found: ${numFound}`;
     docs.map((singleTitle) => {
       console.log(singleTitle);
-      const {author_name, title, first_publish_year: fpy, publisher } = singleTitle;
+      const {author_name, title, first_publish_year: fpy, publisher, cover_i:pic, author_key:author } = singleTitle;
       const bookshow = document.getElementById('bookShow');
+      // bookshow.innerHTML = ``;
     const book = document.createElement('div');
     book.classList.add('col');
     // =======> need to set border of colume element 
     book.innerHTML = `
     <div class="card p-2 ms-3 shadow">
+    <div>
+    <img src="https://covers.openlibrary.org/b/id/${pic}-M.jpg" class="card-img-top" alt="no image" />
+    </div>
     <h5 class="card-title">Title: ${title}</h5>
     <p>Author Name: ${author_name[0]}</p>
     <p>First Publish: ${fpy}</p>
     <p>Publisher Name: ${publisher[0]}</p>
+    <div>
+    <button onclick="loadAuthor('${author}')" type="button" class="btn btn-outline-success" id="liveAlertBtn">Author Details</button>
+    <div id="liveAlertPlaceholder"></div>
+    </div>
     </div>
     `;
     bookshow.appendChild(book);
     });
-    // =========>some other data need to be show 
     
     
+  
 
     
 };
+const loadAuthor = (author_id) => {
+  console.log(author_id);
+
+  fetch(`https://openlibrary.org/authors/${author_id}.json`)
+  .then(res => res.json())
+  .then(data => showAuthor(data))
+ } ;
+
+ const showAuthor = (author) => {
+  console.log(author);
+ };
