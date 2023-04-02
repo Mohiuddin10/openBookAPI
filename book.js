5341683410163072c4a31c1a4883426c2dd26406
Mohiuddin10 <sj.ctg6737@gmail.com>
2023-04-02 12:25:34 +0600

    data load and shadow added

--- book.js
+++ book.js
@@ -18,10 +18,12 @@ const showBook =(data) => {
     book.classList.add('col');
     // =======> need to set border of colume element 
     book.innerHTML = `
-    <h3>Title: ${title}</h3>
+    <div class="card p-2 ms-3 shadow">
+    <h5 class="card-title">Title: ${title}</h5>
     <p>Author Name: ${author_name[0]}</p>
     <p>First Publish: ${fpy}</p>
-    <p>publisher Name: ${publisher[0]}</p>
+    <p>Publisher Name: ${publisher[0]}</p>
+    </div>
     `;
     bookshow.appendChild(book);
     });
